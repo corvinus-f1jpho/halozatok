e68fd4497e0d31626409d51b1fb164bdf27f81b1
Zsófia Utasi <zsofia.utasi@stud.uni-corvinus.hu>
2021-04-25 22:52:26 +0200

    9. gyakorlat feladatai

--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -16,12 +16,14 @@ function letöltésBefejeződött(d) {
 }
 
 
-function kérdésMegjelenítés(kérdés) {
-    document.getElementById("kérdés_szöveg").innerHTML = kérdések[kérdés].questionText;
-    document.getElementById("válasz1").innerHTML = kérdések[kérdés].answer1;
-    document.getElementById("válasz2").innerHTML = kérdések[kérdés].answer2;
-    document.getElementById("válasz3").innerHTML = kérdések[kérdés].answer3;
-    document.getElementById("kép1").src = "https://szoft1.comeback.hu/hajo/" + kérdések[kérdés].image;
+function kérdésMegjelenítés(kérdés)
+{
+        console.log(kérdés);
+        document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+        document.getElementById("válasz1").innerText = kérdés.answer1
+        document.getElementById("válasz2").innerText = kérdés.answer2
+        document.getElementById("válasz3").innerText = kérdés.answer3
+        document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;         
 }
 
 window.onload = function () {
@@ -95,3 +97,35 @@ window.onload = function () {
         document.getElementById("válasz3").style.pointerEvents = 'none';
     }
 }
+
+function APIteszt()
+{    fetch('/questions/4')
+        .then(response => response.json())
+        .then(data => console.log(data)
+        );
+}
+
+function válaszFeldolgozás()
+{
+    fetch('/questions/1')
+        .then(response => response.json())
+        .then(data => kérdésMegjelenítés(data)
+        );
+}
+
+function kérdésBetöltés(id)
+{
+    fetch(`/questions/${id}`)
+        .then(response =>
+        {
+            if (!response.ok)
+            {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else
+            {
+                    return response.json()
+            }
+        }
+     ).then(data => kérdésMegjelenítés(data));
+}    
